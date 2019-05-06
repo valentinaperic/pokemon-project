993110ffcb599edd71cec3287d9eacd40c071a14
Amelia <wattenberger@gmail.com>
2019-05-06 19:40:02 -0400

    SearchPokemon changes, and save pokemon in App state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 
 // App Components
 import SearchPokemon from './components/SearchPokemon.js';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to the Great World of Pokemon</h1>
-      <SearchPokemon />
-      <div id="pokemonInformation"></div>
-    </div>
-  ); 
-} 
+class App extends Component {
+  state = {
+    pokemon: null
+  }
 
+  onPokemonChange = pokemon => {
+    this.setState({ pokemon })
+  }
 
+  render() {
+    const { pokemon } = this.state
 
-
-
-
-
+    return (
+      <div className="App">
+        <h1>Welcome to the Great World of Pokemon</h1>
+        <SearchPokemon onPokemonChange={this.onPokemonChange} />
+        {!!pokemon && (
+          <div>
+            <h1>
+              { pokemon.name }
+            </h1>
+            <img alt="pokemon species" src={pokemon.sprites.front_female} />
+            <p>Height: { pokemon.height }</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/SearchPokemon.js
+++ src/components/SearchPokemon.js
@@ -1,47 +1,61 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
 
-class SearchPokemon extends React.Component { 
+class SearchPokemon extends React.Component {
     constructor() {
       super();
 
-      this.SearchPokemon = this.SearchPokemon.bind(this);
+      this.onSearchPokemon = this.onSearchPokemon.bind(this);
       this.state = {
-        search: ''
+        search: '',
+        error: false,
       };
     }
 
-    SearchPokemon(e) {
-
-      e.preventDefault();
-      
-      const url = fetch(`http://pokeapi.co/api/v2/pokemon/{e}`);
-      
-      const pokemon = document.querySelector("#pokemonInformation");
-     
-        url 
-        .then(function(response) {
-            const promiseResponse = response.json();
-            return promiseResponse;
-    
-        })
-        .then(function(promiseResponse) {
-            const pokemonName = document.createElement("h1");
-            pokemonName.innerText = promiseResponse.name;
-            pokemon.appendChild(pokemonName);
-        });
+    static propTypes = {
+      onPokemonChange: PropTypes.func,
     }
-  
+
+    setSearchValue = e => {
+      const value = e.target.value
+      this.setState({ search: value, error: false })
+    }
+
+    onSearchPokemon(e) {
+      e.preventDefault();
+      this.setState({ error: false })
+
+      const url = fetch(`http://pokeapi.co/api/v2/pokemon/${this.state.search}`);
+
+      url
+      .then(response => {
+        if (!response.ok) throw new Error()
+        return response.json();
+      })
+      .then(responseData => {
+        this.props.onPokemonChange(responseData);
+      })
+      .catch(() => {
+        this.setState({ error: true })
+
+      })
+    }
+
     render() {
       return (
-        <div>
-          <input type="text" value={this.state.search} onChange={event => this.SearchPokemon(event.target.value)}></input>
-          <button onClick={this.SearchPokemon}>Search</button>
-        </div>
- 
+        <form>
+          <input type="text" value={this.state.search} onChange={this.setSearchValue}></input>
+          <button type="submit" onClick={this.onSearchPokemon}>Search</button>
+
+          {this.state.error && (
+            <div>Something went wrong</div>
+          )}
+        </form>
+
       );
     }
   }
-  
-  
+
+
 export default SearchPokemon;
